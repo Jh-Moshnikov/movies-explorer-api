d5b97655eec56bddcd63a0f07f5f9b80b2387ede
Jh_Moshnikov <Jh.Moshnikov@yandex.ru>
2023-07-17 14:11:01 +0300

    настроена работа сервера локально

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
+// eslint-disable-next-line no-unused-expressions
+require('dotenv').config;
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { errors } = require('celebrate');
+const router = require('./routers');
+const errorHandler = require('./middlewares/errorHandler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const cors = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect('mongodb://127.0.0.1/bitfilmsdb');
+app.use(requestLogger);
+app.use(cors);
+app.use(router);
+app.use(errorLogger);
+app.use(errors());
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
