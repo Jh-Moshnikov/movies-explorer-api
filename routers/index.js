4da25b46bd636068f24382a47f2d4d1223423cc2
Jh_Moshnikov <Jh.Moshnikov@yandex.ru>
2023-08-15 19:58:57 +0300

    изменил роут

--- routers/index.js
+++ routers/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const auth = require('../middlewares/auth');
 const userRoutes = require('./users');
-const movieRoutes = require('./movies');
+const cardRoutes = require('./movies');
 const wrongRoutes = require('./wrong');
 const { validationLogin, validationCreateUser } = require('../middlewares/getValidation');
 const { createUser, login } = require('../controllers/user');
@@ -10,7 +10,7 @@ router.post('/signin', validationLogin, login);
 router.post('/signup', validationCreateUser, createUser);
 router.use(auth);
 router.use(userRoutes);
-router.use(movieRoutes);
+router.use(cardRoutes);
 router.use(wrongRoutes);
 
 module.exports = router;
